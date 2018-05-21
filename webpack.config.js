881b8e3cc9c58b34655f3fb5f8f1bd097a58abb3
李杰 <lijie-02@newrank.cn>
2018-05-21 18:13:15 +0800

    更新

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,51 @@
-"use strict";
-var path = require("path");
-var webpack = require("webpack");
-var HtmlWebpackPlugin = require("html-webpack-plugin");
-var VueLoaderPlugin = require("vue-loader/lib/plugin");
+'use strict';
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var VueLoaderPlugin = require('vue-loader/lib/plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+var UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+var type = process.env['npm_config_type'] || 'production';
+console.log('--------------');
+console.log(type);
 function resolve(dir) {
   return path.join(__dirname, dir);
 }
 module.exports = {
-  mode: "development",
-  entry: "./src/main.ts",
+  mode: 'production',
+  entry: './src/main.ts',
   output: {
-    path: path.resolve(__dirname, "./dist"),
-    publicPath: "./",
-    filename: "js/[name].[hash:7].js"
+    path: path.resolve(__dirname, './dist'),
+    publicPath: './',
+    filename: 'js/[name].[hash:7].js'
   },
   resolve: {
-    extensions: [".js", ".vue", ".json", "less"],
+    extensions: ['.js', '.vue', '.json', 'less'],
     alias: {
-      vue$: "vue/dist/vue.esm.js",
-      "@": resolve("src")
+      vue$: 'vue/dist/vue.esm.js',
+      '@': resolve('src')
     }
   },
   module: {
     rules: [
       {
         test: /\.vue$/,
-        loader: "vue-loader",
+        loader: 'vue-loader',
         options: {
           loaders: {
             // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
             // the "scss" and "sass" values for the lang attribute to the right configs here.
             // other preprocessors should work out of the box, no loader config like this necessary.
-            scss: "vue-style-loader!css-loader!sass-loader",
-            sass: "vue-style-loader!css-loader!sass-loader?indentedSyntax"
+            scss: 'vue-style-loader!css-loader!sass-loader',
+            sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
           }
           // other vue-loader options go here
         }
       },
       {
         test: /\.tsx?$/,
-        loader: "ts-loader",
+        loader: 'ts-loader',
         exclude: /node_modules/,
         options: {
           appendTsSuffixTo: [/\.vue$/]
@@ -47,46 +53,59 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        loader: ["vue-style-loader", "css-loader"]
+        // loader: ['vue-style-loader', 'css-loader']
+        use: ExtractTextPlugin.extract({
+          use: ['css-loader'],
+          fallback: 'vue-style-loader'
+        })
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
-        loader: "file-loader",
+        loader: 'file-loader',
         options: {
-          name: "[name].[ext]?[hash]"
+          name: '[name].[ext]?[hash]'
         }
       }
     ]
   },
   devServer: {
-    contentBase: "./dist",
+    contentBase: './dist',
     historyApiFallback: true,
     noInfo: false,
     compress: true,
-    port: "9999"
+    host: 'localhost',
+    port: '9999',
+    hot: true
   },
   performance: {
     hints: false
   },
-  devtool: "#eval-source-map",
+  devtool: '#eval-source-map',
   plugins: [
+    // new CleanWebpackPlugin(['dist'], { root: __dirname, verbose: true }),
     new HtmlWebpackPlugin({
-      template: "index.html"
+      template: 'index.html'
     }),
-    new VueLoaderPlugin()
+    new VueLoaderPlugin(),
+    new ExtractTextPlugin({
+      // filename: 'css/[name].[contenthash].css',
+      filename: 'css/app.css',
+      allChunks: true
+    })
   ]
 };
 
-if (process.env.NODE_ENV === "production") {
-  module.exports.devtool = "#source-map";
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map';
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
+      sourceMap: false,
       compress: {
         warnings: false
       }
     }),
+    new UglifyJsPlugin({}),
     new webpack.LoaderOptionsPlugin({
       minimize: true
     })
